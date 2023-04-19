7398462c5843098e8d9b0cdf3c748bb4824cfae6
Carles Pueyo <cpcastells@gmail.com>
2023-04-19 19:52:10 +0200

    Create component class

---
+++ src/ts/types/types.ts
@@ -0,0 +1,17 @@
+export interface SeriesStructure {
+  id: number;
+  name: string;
+  creator: string;
+  year: number;
+  poster: string;
+  isWatched: boolean;
+  score: 0 | 1 | 2 | 3 | 4 | 5;
+  emmies: number;
+}
+
+export type Series = SeriesStructure[];
+
+export interface ComponentStructure {
+  element: Element;
+  renderHtml(): void;
+}
